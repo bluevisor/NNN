c173aa2abc0dddfdea525f96b4b3cd2a3c5bb91f
John Zheng <bluevisor@gmail.com>
2025-04-01 05:11:32 -0700

    Initial Commit

---
+++ NNN/NNNApp.swift
@@ -0,0 +1,17 @@
+//
+//  NNNApp.swift
+//  NNN
+//
+//  Created by John Zheng on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct NNNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
